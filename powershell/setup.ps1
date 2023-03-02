d2885e4f20009a84a0cf65dba9ecabfaa08da59a
Sander van 't Einde <sandervanteinde@gmail.com>
2023-03-02 17:13:16 +0100

    Update setup.ps1

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -1,9 +1,14 @@
+# Setup oh-my-posh
+
 winget install JanDeDobbeleer.OhMyPosh -s winget
 
-## Restart to refresh PATH
+# Restart to refresh PATH then run the next part.
 
 mkdir C:\oh-my-posh
 
 curl -o C:\oh-my-posh\style.json https://raw.githubusercontent.com/sandervanteinde/settings/main/powershell/style.json
 
-Add-Content $PROFILE "oh-my-posh init pwsh --config 'C:\oh-my-posh\style.json' | Invoke-Expression"
+Add-Content $PROFILE "`noh-my-posh init pwsh --config 'C:\oh-my-posh\style.json' | Invoke-Expression"
+
+Install-Module -Name Terminal-Icons -Repository PSGallery
+Add-Content $PROFILE "`nImport-Module -Name Terminal-Icons"
