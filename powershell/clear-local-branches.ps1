7723502326737032bda4a3cbbf161f4b62b2e42e
Sander van 't Einde <sandervanteinde@gmail.com>
2023-12-08 15:48:56 +0100

    Create clear-local-branches.ps1

---
+++ powershell/clear-local-branches.ps1
@@ -0,0 +1,30 @@
+git fetch
+$currentBranch = git branch --show-current
+$localBranches = git branch | ForEach-Object { $_.Trim() -replace '^\* ', '' }
+$remoteBranches = git branch -r | ForEach-Object { $_.Trim() -replace '^origin/', '' }
+
+$localBranches | ForEach-Object {
+    $localBranch = $_
+    
+    $matchFound = $false
+    $remoteBranches | ForEach-Object {
+        $remoteBranch = $_
+        if ($localBranch -eq $remoteBranch) {
+            $matchFound = $true
+        }
+    }
+
+    if (-not $matchFound) {
+        if($localBranch -eq $currentBranch) {
+            Write-Host "Skipping deletion of the currently checked out branch $localBranch. It is active branch"
+            continue
+        }
+        Write-Host "Branch to delete: $localBranch. Are you sure? (Y/N)"
+        $response = Read-Host
+        if ($response -eq "Y") {
+            git branch -d $localBranch
+        } else {
+            Write-Host "Skipping deletion of $localBranch"
+        }
+    }
+}
